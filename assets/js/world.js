660c7da1e61330040183f2a998943278c8b483ad
nik121 <nik_koshcheyev@hotmail.com>
2014-12-01 14:59:20 -0500

    Revert "Revert "Imp Attacking + Grunt Sounds""
    This reverts commit 885e34f93a141c662c85fa4d759a4d430a796afc.

--- assets/js/world.js
+++ assets/js/world.js
@@ -10,6 +10,7 @@ var World = {
 	// different POI-Marker assets
 	enemyDrawableIdle: null,
 	enemyDrawableDead: null,
+	enemyDrawableAttacking: null,
 	generatorDrawableAlive: null,
 	generatorDrawableDestroyed: null,
 	
@@ -60,6 +61,7 @@ var World = {
 		
 		World.enemyDrawableIdle = new AR.ImageResource("assets/imp.png");
 		World.enemyDrawableDead = new AR.ImageResource("assets/deadImp.png");
+		World.enemyDrawableAttacking = new AR.ImageResource("assets/impAttacking.png");
 		
 		var spawnTimer = setInterval(function () {spawn()}, (((Math.random() * 5) + 5) * 1000));
 		
